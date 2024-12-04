cbefc4fbed95101c1fd53b13c0fc20cf3a53a6ea
Camp <camp.fred@hotmail.com>
2024-12-04 09:59:47 -0500

    Implement handleLink method

--- src/handler/music.ts
+++ src/handler/music.ts
@@ -12,7 +12,7 @@ interface MusicHandler {
 }
 
 export class OdesliMusicHandler implements {
-    readonly _base_url: string
+    readonly _base_url: URL
     readonly _api_key: string
 
     constructor(base_url: string, api_key?: string) {
@@ -21,7 +21,10 @@ export class OdesliMusicHandler implements {
             this._api_key = api_key
     }
 
-    handleLink(link: URL): URL {
-        
+    async handleLink(link: URL): URL {
+        request_url: URL = this._base_url
+        request_url.searchParams.append("url", encodeURI(link.toString()))
+        response = fetch(request_url.toString())
+        return new URL(await response.json()["pageUrl"])
     }
 }
