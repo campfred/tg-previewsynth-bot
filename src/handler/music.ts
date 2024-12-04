c532c87984667220d3d20eb97ae29c174206dacb
Camp <camp.fred@hotmail.com>
2024-12-04 09:41:17 -0500

    Start implementation of Odesli music handler

--- src/handler/music.ts
+++ src/handler/music.ts
@@ -1,6 +1,6 @@
 interface MusicHandler {
-    _base_url: string
-    _api_key: string
+    readonly _base_url: string
+    readonly _api_key: string
 
     # constructor(base_url: string, api_key?: string) {
     #     this._base_url = base_url
@@ -9,4 +9,19 @@ interface MusicHandler {
     # }
 
     handleLink(link: URL): URL
+}
+
+export class OdesliMusicHandler implements {
+    readonly _base_url: string
+    readonly _api_key: string
+
+    constructor(base_url: string, api_key?: string) {
+        this._base_url = base_url
+        if (api_key !== undefined)
+            this._api_key = api_key
+    }
+
+    handleLink(link: URL): URL {
+        
+    }
 }
