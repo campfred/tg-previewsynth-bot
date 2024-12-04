83de2b1c487932b2970ef1b34aae2bf43a3a2dba
Camp <camp.fred@hotmail.com>
2024-12-04 08:24:08 -0500

    Bégin writing music handling

---
+++ src/handler/music.ts
@@ -0,0 +1,13 @@
+class MusicHandler {
+    _base_url: string
+    _api_key: string
+
+    constructor(base_url: string, api_key?: string) {
+        this._base_url = base_url
+        if (api_key !== undefined)
+            this._api_key = api_key
+    }
+
+    handleLink(link: URL): URL {
+    }
+}
