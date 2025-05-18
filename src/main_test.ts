9c2ec2e219bd144b76841f16589168e5bac4c00b
Camp <camp.fred@hotmail.com>
2025-05-18 14:33:13 -0400

    Rework tests to account new destinations

--- src/main_test.ts
+++ src/main_test.ts
@@ -2,151 +2,121 @@ import { assert, assertEquals } from "@std/assert"
 import { SimpleLinkConverter, SimpleLinkConverterSettings } from "./converters/simple.ts"
 import { OdesliMusicConverter } from "./converters/music.ts"
 
-type TestData = {
+type TestPlan = {
+	Link: URL,
+	Result: URL
 	Converter: SimpleLinkConverter
-	Links: {
-		Reduced: URL
-		Full: URL
-		Dirty: URL
-		Alternate: URL
-		AlternateDirty: URL
-		Converted: URL
-	}
 }
 
-const TestFurAffinity: TestData = {
-	Converter: new SimpleLinkConverter("FurAffinity", [new URL("https://furaffinity.net/")], [], new URL("https://xfuraffinity.net/")),
-	Links: {
-		Reduced: new URL("https://furaffinity.net/view/58904471/"),
-		Full: new URL("https://www.furaffinity.net/view/58904471/"),
-		Dirty: new URL("https://www.furaffinity.net/view/58904471/?testy=testtest"),
-		Alternate: new URL("https://sfw.furaffinity.net/view/58904471/"),
-		AlternateDirty: new URL("https://sfw.furaffinity.net/view/58904471/?testy=testtest"),
-		Converted: new URL("https://xfuraffinity.net/view/58904471/"),
-	},
-}
-
-Deno.test("subdomainsFiltering", (): void =>
+Deno.test("Subdomains filtering", (): void =>
 {
 	assertEquals(SimpleLinkConverter.filterOutSubdomains(new URL("https://www.domain.test")), new URL("https://domain.test"))
 })
 
-Deno.test("linkExpanding", async (): Promise<void> =>
+const FurAffinityConverter: SimpleLinkConverter = new SimpleLinkConverter("FurAffinity", [new URL("https://furaffinity.net/")], [], [new URL("https://xfuraffinity.net/")])
+
+Deno.test("Link expanding", async (): Promise<void> =>
 {
-	assertEquals(await TestFurAffinity.Converter.expandLink(TestFurAffinity.Links.Reduced), TestFurAffinity.Links.Full)
+	const Link: URL = new URL("https://furaffinity.net/view/58904471/")
+	const Result: URL = new URL("https://www.furaffinity.net/view/58904471/")
+	assertEquals(await FurAffinityConverter.expandLink(Link), Result)
 })
 
-Deno.test("linkCleaning", (): void =>
+Deno.test("Link cleaning", (): void =>
 {
-	assertEquals(TestFurAffinity.Converter.cleanLink(TestFurAffinity.Links.Dirty), TestFurAffinity.Links.Full)
+	const Link: URL = new URL("https://furaffinity.net/view/58904471/?testy=testtest")
+	const Result: URL = new URL("https://furaffinity.net/view/58904471/")
+	assertEquals(FurAffinityConverter.cleanLink(Link), Result)
 })
 
-Deno.test("linkConversion", (): void =>
+Deno.test("Link conversion only", (): void =>
 {
-	assertEquals(TestFurAffinity.Converter.convertLink(SimpleLinkConverter.filterOutSubdomains(TestFurAffinity.Links.Full)), TestFurAffinity.Links.Converted)
+	const Link: URL = new URL("https://furaffinity.net/view/58904471/")
+	const Result: URL = new URL("https://xfuraffinity.net/view/58904471/")
+	assertEquals(FurAffinityConverter.convertLink(Link), Result)
 })
 
-Deno.test("linkConversionWithSubdomainsAndParams", async (): Promise<void> =>
+Deno.test("Full chain conversion", async (): Promise<void> =>
 {
-	assertEquals(await TestFurAffinity.Converter.parseLink(TestFurAffinity.Links.Alternate), TestFurAffinity.Links.Converted)
+	const Link: URL = new URL("https://www.furaffinity.net/view/58904471/?testy=testtest")
+	const Result: URL = new URL("https://xfuraffinity.net/view/58904471/")
+	assertEquals(await FurAffinityConverter.parseLink(Link, FurAffinityConverter.destinations[0].hostname), Result)
 })
 
 Deno.test("TikTok-specific test case", async (): Promise<void> =>
 {
-	const Converter: SimpleLinkConverter = new SimpleLinkConverter("TikTok", [new URL("https://tiktok.com/"), new URL("https://vm.tiktok.com/")], [], new URL("https://vxtiktok.com/"))
-	const ShareLink = new URL("https://vm.tiktok.com/ZMhtQT3Yf/")
-	const ConvertedLink = new URL("https://vxtiktok.com/@mokastagelight/video/7427030188069883179")
-
-	assertEquals(await Converter.parseLink(ShareLink), ConvertedLink)
+	const Link: URL = new URL("https://vm.tiktok.com/ZMhtQT3Yf/")
+	const Result: URL = new URL("https://vxtiktok.com/@/video/7427030188069883179")
+	const Converter: SimpleLinkConverter = new SimpleLinkConverter("TikTok", [new URL("https://tiktok.com/")], [], [new URL("https://vxtiktok.com/")])
+	assertEquals(await Converter.parseLink(Link, Converter.destinations[0].hostname), Result)
 })
 
 Deno.test("Reddit-specific test case", async function (): Promise<void>
 {
-	const Converter: SimpleLinkConverter = new SimpleLinkConverter("Reddit", [new URL("https://reddit.com"), new URL("https://redd.it")], [], new URL("https://rxddit.com"))
-	const ShareLink = new URL("https://www.reddit.com/r/cats/s/pGdqltuTkJ")
-	const ExpectedLink = new URL("https://rxddit.com/r/cats/comments/1ij688f/ill_doodle_your_cat_here_we_go/")
+	const Link: URL = new URL("https://reddit.com/r/cats/comments/1ij688f/ill_doodle_your_cat_here_we_go/")
+	const Result: URL = new URL("https://vxreddit.com/r/cats/comments/1ij688f/ill_doodle_your_cat_here_we_go/")
+	const Converter: SimpleLinkConverter = new SimpleLinkConverter("Reddit", [new URL("https://reddit.com")], [], [new URL("https://vxreddit.com")])
+	assertEquals(await Converter.parseLink(Link, Converter.destinations[0].hostname), Result)
+})
 
-	const ConvertedLink: URL | null = await Converter.parseLink(ShareLink)
-	assertEquals(ConvertedLink, ExpectedLink)
+Deno.test("YouTube-specific constraint", function (): void
+{
+	const Converter: SimpleLinkConverter = new SimpleLinkConverter("YouTube", [new URL("https://youtube.com/watch")], [], [new URL("https://yfxtube.com/watch")], youtubeConversionSettings)
+	const Link: URL = new URL("https://www.youtube.com/shorts/_r53PoMVZTQ?feature=share")
+	assert(!Converter.isSupported(Link))
 })
 
 const youtubeConversionSettings: SimpleLinkConverterSettings = { expand: false, preserveSearchParams: ["v", "t"] }
 Deno.test("YouTube-specific test case", async function (): Promise<void>
 {
-	const Converter: SimpleLinkConverter = new SimpleLinkConverter("YouTube", [new URL("https://youtube.com/watch")], [], new URL("https://yfxtube.com/watch"), youtubeConversionSettings)
-	const ShareLink = new URL("https://www.youtube.com/watch?v=0yiwxIuXmdk&t=355s")
-	const ExpectedLink = new URL("https://yfxtube.com/watch?v=0yiwxIuXmdk&t=355s")
-
-	const ConvertedLink: URL | null = await Converter.parseLink(ShareLink)
-	assertEquals(ConvertedLink, ExpectedLink)
-})
-
-Deno.test("YouTube-specific constraint", function (): void
-{
-	const Converter: SimpleLinkConverter = new SimpleLinkConverter("YouTube", [new URL("https://youtube.com/watch")], [], new URL("https://yfxtube.com/watch"), youtubeConversionSettings)
-	const ShortLink = new URL("https://www.youtube.com/shorts/_r53PoMVZTQ?feature=share")
-
-	assert(!Converter.isSupported(ShortLink))
+	const Link: URL = new URL("https://www.youtube.com/watch?v=0yiwxIuXmdk&t=355s")
+	const Result: URL = new URL("https://yfxtube.com/watch?v=0yiwxIuXmdk&t=355s")
+	const Converter: SimpleLinkConverter = new SimpleLinkConverter("YouTube", [new URL("https://youtube.com/watch")], [], [new URL("https://yfxtube.com/watch")], youtubeConversionSettings)
+	assertEquals(await Converter.parseLink(Link, Converter.destinations[0].hostname), Result)
 })
 
 Deno.test("YouTu.be-specific test case", async function ()
 {
-	const Converter: SimpleLinkConverter = new SimpleLinkConverter("YouTu.be", [new URL("https://youtu.be")], [], new URL("https://fxyoutu.be"), youtubeConversionSettings)
-	const ShareLink = new URL("https://youtu.be/KBYvH3myqyg?t=237&si=FHSrjDGQz3-8UawQ")
-	const ExpectedLink = new URL("https://fxyoutu.be/KBYvH3myqyg?t=237")
-
-	const ConvertedLink: URL | null = await Converter.parseLink(ShareLink)
-	assertEquals(ConvertedLink, ExpectedLink)
+	const Link: URL = new URL("https://youtu.be/KBYvH3myqyg?t=237&si=FHSrjDGQz3-8UawQ")
+	const Result: URL = new URL("https://fxyoutu.be/KBYvH3myqyg?t=237")
+	const Converter: SimpleLinkConverter = new SimpleLinkConverter("YouTu.be", [new URL("https://youtu.be")], [], [new URL("https://fxyoutu.be")], youtubeConversionSettings)
+	assertEquals(await Converter.parseLink(Link, Converter.destinations[0].hostname), Result)
 })
 
-Deno.test("FurTrack-specific constraint", function (): void
-{
-	const Converter: SimpleLinkConverter = new SimpleLinkConverter("FurTrack", [new URL("https://furtrack.com/p/")], [], new URL("https://furtrack.owo.lgbt/p/"))
-	const NavLink = new URL("https://www.furtrack.com/index/event:furxmas2025/880404")
-
-	assert(!Converter.isSupported(NavLink))
-})
-
-const furtrackRegExp: RegExp = /^https:\/\/(www\.)?furtrack\.com\/(\w+\/)?(\w+[:/])?\w+(\(\w+\))?\//i
-const FurTrackConverter: SimpleLinkConverter = new SimpleLinkConverter("FurTrack", [new URL("https://furtrack.com/p/")], [furtrackRegExp], new URL("https://furtrack.owo.lgbt/p/"))
+const FurTrackConverter: SimpleLinkConverter = new SimpleLinkConverter("FurTrack", [new URL("https://furtrack.com/p/")], [/^https:\/\/(www\.)?furtrack\.com\/(\w+\/)?(\w+[:/])?\w+(\(\w+\))?\//i], [new URL("https://furtrack.owo.lgbt/p/")])
 
 Deno.test("FurTrack index translation", async function (): Promise<void>
 {
 	const NavLink = new URL("https://www.furtrack.com/index/character:yin_(wolf)/880404")
 	const ConvertedLink = new URL("https://furtrack.owo.lgbt/p/880404")
-
-	assertEquals(await FurTrackConverter.parseLink(NavLink), ConvertedLink)
+	assertEquals(await FurTrackConverter.parseLink(NavLink, FurTrackConverter.destinations[0].hostname), ConvertedLink)
 })
 
 Deno.test("FurTrack user favs page translation", async function (): Promise<void>
 {
 	const NavLink = new URL("https://www.furtrack.com/user/bark/likes/691174")
 	const ConvertedLink = new URL("https://furtrack.owo.lgbt/p/691174")
-
-	assertEquals(await FurTrackConverter.parseLink(NavLink), ConvertedLink)
+	assertEquals(await FurTrackConverter.parseLink(NavLink, FurTrackConverter.destinations[0].hostname), ConvertedLink)
 })
 
 Deno.test("FurTrack user fursuiting page translation", async function (): Promise<void>
 {
 	const NavLink = new URL("https://www.furtrack.com/user/bark/fursuiting/888628")
 	const ConvertedLink = new URL("https://furtrack.owo.lgbt/p/888628")
-
-	assertEquals(await FurTrackConverter.parseLink(NavLink), ConvertedLink)
+	assertEquals(await FurTrackConverter.parseLink(NavLink, FurTrackConverter.destinations[0].hostname), ConvertedLink)
 })
 
 Deno.test("FurTrack user photography page translation", async function (): Promise<void>
 {
 	const NavLink = new URL("https://www.furtrack.com/user/bark/photography/753398")
 	const ConvertedLink = new URL("https://furtrack.owo.lgbt/p/753398")
-
-	assertEquals(await FurTrackConverter.parseLink(NavLink), ConvertedLink)
+	assertEquals(await FurTrackConverter.parseLink(NavLink, FurTrackConverter.destinations[0].hostname), ConvertedLink)
 })
 
 Deno.test("Music-specific test case", async (): Promise<void> =>
 {
-	const Converter: OdesliMusicConverter = new OdesliMusicConverter("Odesli", [new URL("https://open.spotify.com")], new URL("https://song.link"))
+	const Converter: OdesliMusicConverter = new OdesliMusicConverter("Odesli", [new URL("https://open.spotify.com")], [new URL("https://song.link")])
 	const SpotifyShareLink = new URL("https://open.spotify.com/intl-fr/track/4zbInBD4rY7tYPJ16LVxdh?si=3ca28df1bfa044db")
 	const OdesliConvertedLink = new URL("https://song.link/s/4zbInBD4rY7tYPJ16LVxdh")
-
-	assertEquals(await Converter.parseLink(SpotifyShareLink), OdesliConvertedLink)
+	assertEquals(await Converter.parseLink(SpotifyShareLink, Converter.destinations[0].hostname), OdesliConvertedLink)
 })
